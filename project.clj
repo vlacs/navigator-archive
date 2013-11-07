70951ba21771c92af82c64066b4b024ee4b46239
Matt Oquist <moquist@majen.net>
2013-11-06 22:38:26 -0500

    add comment to explain where to get jdbc-pg-init

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+
+                 ;; see https://github.com/vlacs/jdbc-pg-init
                  [jdbc-pg-init "0.1.1-SNAPSHOT"]
-                 ;[org.clojure/java.jdbc "0.3.0-beta1"]
+                 ;;[org.clojure/java.jdbc "0.3.0-beta1"]
                  [org.clojure/tools.namespace "0.2.4"]
                  [postgresql/postgresql "8.4-702.jdbc4"]])
