a44b900e8c6efc562732cbb54d01ed9f339199af
Matt Oquist <moquist@majen.net>
2013-11-07 12:55:14 -0500

    add org.clojure/tools.cli dep and define :main

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.cli "0.2.4"]
 
                  ;; see https://github.com/vlacs/jdbc-pg-init
                  [jdbc-pg-init "0.1.1-SNAPSHOT"]
                  ;;[org.clojure/java.jdbc "0.3.0-beta1"]
                  [org.clojure/tools.namespace "0.2.4"]
-                 [postgresql/postgresql "8.4-702.jdbc4"]])
+                 [postgresql/postgresql "8.4-702.jdbc4"]]
+  :main ^:skip-aot aspire.core)
