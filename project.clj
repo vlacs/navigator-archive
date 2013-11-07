1df814ae34aa7c0521e236096017e2bda9344bb6
Matt Oquist <moquist@majen.net>
2013-11-06 22:29:53 -0500

    Update dependencies and description.

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject aspire "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Create, read, and update a PostgreSQL DB for Aspire.
+  PostgreSQL will be replaced by Datomic in a future version of
+  Aspire."
+  :url "https://github.com/vlacs/aspire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [jdbc-pg-init "0.1.1-SNAPSHOT"]
+                 ;[org.clojure/java.jdbc "0.3.0-beta1"]
+                 [org.clojure/tools.namespace "0.2.4"]
+                 [postgresql/postgresql "8.4-702.jdbc4"]])
