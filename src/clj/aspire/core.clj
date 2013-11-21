f636e28fded72b7e5eb896c124c46e52e1bf0ed9
Matt Oquist <moquist@majen.net>
2013-11-20 22:08:53 -0500

    Update to allow developer workflow using tools.namespace.

--- src/clj/aspire/core.clj
+++ src/clj/aspire/core.clj
@@ -6,29 +6,62 @@
             [aspire.sqldb :as a-sqldb]
             [aspire.web :as a-web]))
 
-(defn aspire [{:keys [conf-sql-db] :as conf}]
+(defn system
+  "http://thinkrelevance.com/blog/2013/06/04/clojure-workflow-reloaded"
+  [conf]
+  (println :hi)
+  {:conf conf})
+
+(defn start!
+  "Performs side effects to initialize the system, acquire resources,
+  and start it running. Returns an updated instance of the system."
+  [system]
+  (assoc system
+    :sql-db-pool
+    (a-sqldb/default-connection! (get-in system [:conf :conf-sql-db]))
+    :jetty-instance
+    (a-web/run! (get-in system [:conf :conf-web]))))
+
+(defn stop!
+  "Performs side effects to shut down the system and release its
+  resources. Returns an updated instance of the system."
+  [system]
+  (.close (:datasource @(:sql-db-pool system)))
+  (.stop (:jetty-instance system))
+  (dissoc system :sql-db-pool :jetty-instance))
+
+#_(defn aspire [{:keys [conf-sql-db] :as conf}]
   (println :aspire "Do it. Do it now." (dissoc conf-sql-db :password))
-  (a-sqldb/default-connection! conf-sql-db)
   (println (a-sqldb/select! {:select [:*] :from [:comp] :limit 2}))
-  (a-web/run))
+  (a-web/run!))
+
+(defn get-conf! [args]
+  (let [opts (a-cli/get-opts args)
+        conf (a-conf/getconf a-conf/configs opts)]
+    [opts conf]))
 
 (defn -main [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
 
-  (let [opts (a-cli/get-opts args)
-        conf (a-conf/getconf a-conf/configs opts)
-        db (:conf-sql-db conf)]
-    (a-util/output! :opts opts :conf conf :db db)
+  (let [[opts conf] (get-conf! args)
+        db (:conf-sql-db conf)
+        system (system conf)]
+    (a-util/output! :system system :conf conf :db db)
 
     (cond
      (:init-sql opts) (a-sqldb-ddl/init! db) 
      (:zero-out-sql-db opts) (a-sqldb-ddl/print-drop-sql!) 
-     :else (aspire conf))))
+     :else (start! system))))
 
 (comment
 
-  (-main "--conf-sql-db" (format "%s/.aspire/conf-sql-db.edn" (System/getProperty "user.home")))
+  (def home (System/getProperty "user.home"))
+  (def mydir (format "%s/.aspire/" home))
+  (def args ["--verbose"
+             "--conf-sql-db" (format "%s/conf-sql-db.edn" mydir)
+             "--conf-web" (format "%s/conf-web.edn" mydir)])
+  (def system (apply -main args))
 
   )
 
