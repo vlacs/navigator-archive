1801d60189e9c13d920d4eb18a0ddc43495463fa
Matt Oquist <moquist@majen.net>
2014-01-23 22:51:00 -0500

    use the new template, add a placeholder /logout route

--- src/aspire/web.clj
+++ src/aspire/web.clj
@@ -29,10 +29,13 @@
 (defresource resource-base
   :available-media-types ["text/html"]
   :handle-ok (fn [_]
-               (a-tpl/render (a-tpl/base (rand-int 100) "http://google.com" "Bo Jackson"))))
+               (a-tpl/render (a-tpl/base (rand-int 100) "http://google.com" "Bo Jackson"
+                                         "Welcome to VLACS! (make me configurable)" "Check out our learning options below. You'll find projects, courses, and opportunities to learn by pursuing your interests. Join us in your learning adventure! Actually, this text needs to be configured by the admin in some reasonable kind of interface."
+                                         ["Actually, these steps should be configured by the admin in some reasonable kind of interface." "Maybe there will be a step here." "Possibly another step?"]))))
 
 (defroutes app-routes
-  (ANY "/" [] resource-base))
+  (ANY "/" [] resource-base)
+  (ANY "/logout" [] "Nothing here yet but us chickens."))
 
 (def app
   (-> app-routes
