cdaf714d7c9c6a8855c3d644896b6c688acd0c6a
Matt Oquist <moquist@majen.net>
2014-01-29 17:13:48 -0500

    add get-config! and set-config! fns

--- src/aspire/util.clj
+++ src/aspire/util.clj
@@ -1,4 +1,5 @@
-(ns aspire.util)
+(ns aspire.util
+  (:require [aspire.sqldb :as a-sql]))
 
 (defn output!
   "Call output! instead of using prn. Respects --verbose. Returns nil."
@@ -13,3 +14,18 @@
   (let [transform-fn (partial keyword-in-ns target-ns)]
     (map transform-fn (flatten keywords))))
 
+(defn get-config!
+  "Get the specified config key from the DB."
+  [key]
+  (:value
+   (first
+    (a-sql/select! {:select [:value]
+                    :from [:config]
+                    :where [:= :key key]}))))
+
+(defn set-config!
+  "Set the value of the specified config.key in the DB, inserting a new row or updating as appropriate."
+  [key value]
+  (if-let [_ (get-config! key)]
+    (a-sql/update! "UPDATE config SET value = ? WHERE key = ?" [value key])
+    (a-sql/update! "INSERT INTO config (value, key) VALUES (?, ?)" [value key])))
