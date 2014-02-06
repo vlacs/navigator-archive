aec0cafb031e0f86dafffe07dfbf916ae8f063fa
Matt Oquist <moquist@majen.net>
2014-02-06 14:53:12 -0500

    use the new templating fns

--- src/aspire/handlers.clj
+++ src/aspire/handlers.clj
@@ -2,11 +2,6 @@
   (:require [aspire.templates :as a-tpl]
             [aspire.util :as a-util]))
 
-
-(defn config-key! [ctx]
-  (spit "/tmp/expectorate.txt" (str ctx))
-  "config-key!")
-
 (def config-page-keys
   ;; List all the config keys we expect to receive from a particular
   ;; config page.
@@ -20,28 +15,34 @@
      (when (some #{key} (page config-page-keys)) true))
    params))
 
+(defn config-key!
+  "This is intended to be where an AJAX-PUT single config key would be handled."
+  [ctx]
+  (spit "/tmp/expectorate.txt" (str ctx))
+  "This is not implemented yet.")
+
 (defn config-page!
   "Take in a page of configuration updates for the config table.
    Apply the updates to the DB and return the map of results from set-config!."
   [ctx]
   (let [page (get-in ctx [:request :route-params :page])
         params (get-in ctx [:request :form-params])]
-    #_[:params params]
     (map (fn do-config-page! [[k v]]
            (a-util/set-config! (str page "/" k) v))
          (whitelist-config-page-keys (keyword page) params))))
 
+;; Page handlers
+;; -----------------------
 (defn onboarding! [ctx]
-  (let [header (a-tpl/common-header (rand-int 100) "http://google.com" "Bo Jackson")
+  (let [common-snippets (a-tpl/common-snippets ctx)
         greeting (a-util/get-config! "onboarding/greeting")
         greeting-msg (a-util/get-config! "onboarding/greeting-msg")
-        steps (a-util/split-paragraphs (a-util/get-config! "onboarding/steps"))
-        request (:request ctx)]
-    (a-tpl/render (a-tpl/onboarding header greeting greeting-msg steps))))
+        steps (a-util/split-paragraphs (a-util/get-config! "onboarding/steps"))]
+    (a-tpl/onboarding common-snippets greeting greeting-msg steps)))
 
-(defn admin! [_]
-  (let [header (a-tpl/common-header (rand-int 100) "http://google.com" "Jo Backson")
+(defn admin! [ctx]
+  (let [common-snippets (a-tpl/common-snippets ctx)
         greeting (a-util/get-config! "onboarding/greeting")
         greeting-msg (a-util/get-config! "onboarding/greeting-msg")
         steps-str (a-util/get-config! "onboarding/steps")]
-    (a-tpl/render (a-tpl/admin header greeting greeting-msg steps-str))))
+    (a-tpl/admin common-snippets greeting greeting-msg steps-str)))
