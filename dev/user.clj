ecd7cc46d2e44acbda4db2ae72fa5e0b66dcafb5
Matt Oquist <moquist@majen.net>
2014-02-06 14:45:51 -0500

    pull aspire namespaces into the 'user ns with their conventional abbreviations

--- dev/user.clj
+++ dev/user.clj
@@ -6,8 +6,8 @@
             [clojure.test :as test]
             [clojure.tools.namespace.repl :refer (refresh refresh-all)]
             [hickory.core :as hickory]
-            [aspire.core :as aspire.core]
-            [aspire.conf :as aspire.conf]
+            [aspire.core :as a-core]
+            [aspire.conf :as a-conf]
             [aspire.templates :as a-tpl]
             [net.cgrand.enlive-html :as en]
             ))
@@ -21,20 +21,20 @@
         mydir (format "%s/.aspire" home)
         args ["--verbose"
               "--config-path" mydir]
-        [opts conf] (aspire.core/opts-and-conf-from-args args)]
-    (alter-var-root #'system (constantly (aspire.core/system conf)))
+        [opts conf] (a-core/opts-and-conf-from-args args)]
+    (alter-var-root #'system (constantly (a-core/system conf)))
     system))
 
 (defn start!
   "Starts the current development system."
   []
-  (alter-var-root #'system aspire.core/start!))
+  (alter-var-root #'system a-core/start!))
 
 (defn stop!
   "Shuts down and destroys the current development system."
   []
   (alter-var-root #'system
-                  (fn [s] (when s (aspire.core/stop! s)))))
+                  (fn [s] (when s (a-core/stop! s)))))
 
 (defn go
   "Initializes the current development system and starts it running."
