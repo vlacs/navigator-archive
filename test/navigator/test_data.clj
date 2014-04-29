507d9a7a1263e50b0770d157554b7dd73e5acd73
David Zaharee <david@ianus.local.zaharee.com>
2014-04-29 09:20:33 -0400

    more work on test data

--- test/navigator/test_data.clj
+++ test/navigator/test_data.clj
@@ -5,19 +5,19 @@
 
 (defn load-multiple!
   [db-conn entity-type entities]
-  (doseq [ent entities] (navigator/tx-entity! db-conn entity-type ent)))
+  (doall (map #(navigator/tx-entity! db-conn entity-type %) entities)))
 
 (defn load-test-data!
   ([db-conn] (load-test-data! db-conn test-data))
   ([db-conn data]
-     (doseq [[entity-type entities] (partition 2 data)]
-       (load-multiple! db-conn entity-type entities))))
+     (doall (map (fn [[entity-type entities]] (load-multiple! db-conn entity-type entities))
+                 (partition 2 data)))))
 
 (def test-data
   [:comp-tag [{:comp-tag/name "VLACS HS Diploma"
                :comp-tag/version "v1"
                :comp-tag/type "program-of-study"
-               :comp-tag/status "active"}
+               :comp-tag/status :comp-tag.status/active}
               {:comp-tag/name "Mathematics"
                :comp-tag/version "v1"
                :comp-tag/type "area-of-study"
@@ -61,32 +61,41 @@
    :comp [{:comp/id-sk "1"
            :comp/name "Can add vectors"
            :comp/version "v1"
-           :comp/status :comp.status/active}
+           :comp/status :comp.status/active
+           :comp/tags [[:comp-tag/name "VLACS HS DIPLOMA" :comp-tag/version "v1"]
+                       [:comp-tag/name "Mathematics" :comp-tag/version "v1"]]}
           {:comp/id-sk "2"
            :comp/name "Can draw circles"
            :comp/verion "v1"
-           :comp/status :comp.status/active}
+           :comp/status :comp.status/active
+           :comp/tags [[:comp-tag/name "VLACS HS DIPLOMA" :comp-tag/version "v1"]]}
           {:comp/id-sk "3"
            :comp/name "Can say \"hegemon\" in Spanish"
            :comp/version "v1"
-           :comp/status :comp.status/active}
+           :comp/status :comp.status/active
+           :comp/tags [[:comp-tag/name "VLACS HS DIPLOMA" :comp-tag/version "v1"]]}
           {:comp/id-sk "4"
            :comp/name "Can say \"disjunctivist\" in English"
            :comp/version "v1"
-           :comp/status :comp.status/archived}
+           :comp/status :comp.status/archived
+           :comp/tags [[:comp-tag/name "VLACS HS DIPLOMA" :comp-tag/version "v1"]]}
           {:comp/id-sk "5"
            :comp/name "Can write resaurant criticism"
            :comp/version "v1"
-           :comp/status :comp.status/active}
+           :comp/status :comp.status/active
+           :comp/tags [[:comp-tag/name "VLACS HS DIPLOMA" :comp-tag/version "v1"]]}
           {:comp/id-sk "6"
            :comp/name "Can create balloon animals"
            :comp/version "v1"
-           :comp/status :comp.status/active}
+           :comp/status :comp.status/active
+           :comp/tags [[:comp-tag/name "VLACS HS DIPLOMA" :comp-tag/version "v1"]]}
           {:comp/id-sk "7"
            :comp/name "Can say \"disjunctivist\" in English"
            :comp/version "v2"
-           :comp/status :comp.status/active}
+           :comp/status :comp.status/active
+           :comp/tags [[:comp-tag/name "VLACS HS DIPLOMA" :comp-tag/version "v1"]]}
           {:comp/id-sk "8"
            :comp/name "Can compose trios for washboard"
            :comp/version "v1"
-           :comp/status :comp.status/preactive}]])
+           :comp/status :comp.status/preactive
+           :comp/tags [[:comp-tag/name "VLACS HS DIPLOMA" :comp-tag/version "v1"]]}]])
