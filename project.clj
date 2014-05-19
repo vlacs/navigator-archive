a261eb3df188946d8a3c04fd3ba8093b9eb5f6a2
Mike George <mikeyg6754@gmail.com>
2014-05-18 20:08:10 -0400

    update deps, organize project.clj, add voom meta

--- project.clj
+++ project.clj
@@ -5,18 +5,24 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.2.4"]
-                 [com.datomic/datomic-free "0.9.4707"
-                  :exclusions [commons-codec]]
-                 [liberator "0.10.0" :exclusions [hiccup]]
-                 [datomic-schematode "0.1.0-RC1"]
-                 [org.vlacs/timber "0.1.7"
-                  :exclusions [hiccup]]
+
+                 ^{:voom {:repo "https://github.com/vlacs/hatch"}}
                  [org.vlacs/hatch "0.1.2"]
-                 [org.vlacs/helmsman "0.2.5"]
-                 [valip "0.2.0"]]
+                 ^{:voom {:repo "https://github.com/vlacs/helmsman"}}
+                 [org.vlacs/helmsman "0.2.6"]
+                 ^{:voom {:repo "https://github.com/vlacs/timber"}}
+                 [org.vlacs/timber "0.1.7"]
+
+                 [bouncer "0.3.1-beta1"]
+                 [datomic-schematode "0.1.0-RC1"]
+                 [com.datomic/datomic-free "0.9.4707" :exclusions [commons-codec]]
+                 [liberator "0.10.0" :exclusions [hiccup]]]
+
   :resource-paths ["resources"]
   :pedantic? :warn ; :abort
+
   :plugins [[lein-cloverage "1.0.2"]]
+
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [ring "1.2.2"]]}}
