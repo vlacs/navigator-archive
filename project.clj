67d3462971dfc6b19c0141a588ac8fad78126bf9
Matt Oquist <moquist@majen.net>
2014-06-11 13:37:59 -0400

    bump version, voom freshen, add prismatic schema

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.vlacs/navigator "0.1.2-SNAPSHOT"
+(defproject org.vlacs/navigator "0.1.3-SNAPSHOT"
   :description "Navigator supports VLACS' vision for competency-based virtual education."
   :url "https://github.com/vlacs/navigator"
   :license {:name "Eclipse Public License"
@@ -9,14 +9,15 @@
                  ^{:voom {:repo "https://github.com/vlacs/hatch"}}
                  [org.vlacs/hatch "0.1.2"]
                  ^{:voom {:repo "https://github.com/vlacs/helmsman"}}
-                 [org.vlacs/helmsman "0.2.6"]
+                 [org.vlacs/helmsman "0.2.6-20140508_004555-g5275ae2"]
                  ^{:voom {:repo "https://github.com/vlacs/timber"}}
-                 [org.vlacs/timber "0.1.7"]
+                 [org.vlacs/timber "0.1.7-20140603_205620-gd255b55"]
 
                  [bouncer "0.3.1-beta1"]
                  [datomic-schematode "0.1.0-RC1"]
                  [com.datomic/datomic-free "0.9.4707" :exclusions [commons-codec]]
-                 [liberator "0.10.0" :exclusions [hiccup]]]
+                 [liberator "0.10.0" :exclusions [hiccup]]
+                 [prismatic/schema "0.2.2"]]
 
   :resource-paths ["resources"]
   :pedantic? :warn ; :abort
