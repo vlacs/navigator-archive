7097f5bdc3af48aa0349b6fd894d9ce802f6e41a
David Zaharee <dzaharee@vlacs.org>
2014-06-12 15:32:58 -0400

    remove things handled by oarlock

--- src/navigator/schema.clj
+++ src/navigator/schema.clj
@@ -26,32 +26,6 @@
     :dbfns [(constraints/unique :comp-tag :name :version)]}
    {:namespace :comp-tag-disp-ctx
     :attrs [[:name :string]]}
-   {:namespace :perf-asmt
-    :attrs [[:id-sk :string "Shared key from 3rd parties"]
-            [:id-sk-origin :keyword] ; e.g., :moodle, :show-evidence, etc.
-            [:id-sk-with-origin :string "Combined id-sk and id-sk-origin" :db.unique/identity]
-            [:name :string]
-            [:version :string] ; not null
-
-            ;; Example types: :project :course-competency ... TODO: enum?
-            [:type :string] ; not null
-            [:duration-rating-days :bigint]
-            [:comps :ref :many]
-            [:credit-value-numerator :bigint]
-            [:credit-value-denominator :bigint]]
-    :dbfns [(constraints/unique :perf-asmt :name :version :type)
-            (constraints/unique :perf-asmt :id-sk :id-sk-origin)]}
-   {:namespace :task
-    :attrs [[:id-sk :string]
-            [:id-sk-origin :keyword] ; e.g., :moodle, :show-evidence, etc.
-            [:id-sk-with-origin :string "Combined id-sk and id-sk-origin" :db.unique/identity]
-            [:name :string]
-            [:version :string]
-            [:description :string]
-
-            ;; TODO: is this correct?
-            [:comps :ref :many]]
-    :dbfns [(constraints/unique :perf-asmt :id-sk :id-sk-origin)]}
    {:namespace :user2comp
     :attrs [[:sis-user-id :bigint :indexed]
             [:comp :ref :indexed]
@@ -64,20 +38,4 @@
             [:final-score :string]
             [:score-denominator :bigint]
             [:score-type :enum [:numeric :letter :pass-fail :qualitative]] ; enum is what we want here, n'est-ce pas?
-            [:qualitative-notes :string]]}
-
-   {:namespace :user2perf-asmt
-    :attrs [[:user :ref]
-            [:perf-asmt :ref]
-            ;; TODO: finish this
-            [:grade :boolean]]}
-
-   {:namespace :user2task
-    :attrs [[:user :ref]
-            [:task :ref]
-            ;; TODO: finish this
-            [:progress :boolean]]}
-
-   {:namespace :config
-    :attrs [[:key :string :indexed] ; not null
-            [:value :string :indexed]]}])
+            [:qualitative-notes :string]]}])
